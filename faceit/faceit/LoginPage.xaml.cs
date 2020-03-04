f2f1ecc53508fe6b58a87817e4da900cc90178dc
DIEGO MACIEL <diego.maciel2@senaisp.edu.br>
2020-03-04 19:22:44 -0300

    Up do Projeto 4 telas principais

---
+++ faceit/faceit/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace faceit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EsqueceuASenha_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Teste", "Isso é um Teste", "ok");
+        }
+
+        private void Cadastro_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CadastroPJ());
+        }
+    }
+}
