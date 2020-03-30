393acd52e0e9674efeb2e31589be5559d41e201a
DIEGO MACIEL <diego.maciel2@senaisp.edu.br>
2020-03-29 23:28:07 -0300

    UPAFILHODAPUTA

---
+++ FaceIT/FaceIT/Models/FaqModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceIT.Models
+{
+    public class FaqModel
+    {
+        public string Pergunta { get; set; }
+        public bool IsVisible { get; set; }
+        public string Resposta { get; set; }
+    }
+}
---
+++ FaceIT/FaceIT/View/SimpleResetPasswordPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FaceIT.Page;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace FaceIT.Views.Forms
+{
+    /// <summary>
+    /// Page to reset old password
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SimpleResetPasswordPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleResetPasswordPage" /> class.
+        /// </summary>
+        public SimpleResetPasswordPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToRegisterPage(object sender, System.EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new RegisterPage());
+        }
+    }
+}
